1c24ab0bb15a9c0d138f371bafc96e94a34f7d59
Oláh Zoltán <olah.zoltan1k84@gmail.com>
2022-02-16 20:47:39 +0200

    Add messages from UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  
   root 'chatroom#index'
   get 'login', to: 'session#new'
   post 'login', to: 'session#create'
   get 'logout', to: 'session#destroy'
+  post 'message', to: 'messages#create'
 end
