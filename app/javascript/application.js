306ba007e9d977b7c4cd8fb88ecdc39eac1e889e
Oláh Zoltán <olah.zoltan1k84@gmail.com>
2022-02-17 21:52:30 +0200

    Generate a Chatroom channel

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -12,4 +12,4 @@ $(document).on('turbolinks:load', function() {
   $('.message .close').on('click', function() {
       $(this).closest('.message').transition('fade');
     });
-  })
+  })import "./channels"
