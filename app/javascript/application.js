c2c2d6430c0aabdb1a69d7dfeb375b2338dfdd5a
Oláh Zoltán <olah.zoltan1k84@gmail.com>
2022-02-19 17:32:31 +0200

    Beautify input box, functionality and create custom scope

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,15 +1,37 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import Rails from "@rails/ujs"
+import 'jquery'
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
 import "@hotwired/turbo-rails"
 import "controllers"
 import '@doabit/semantic-ui-sass'
 import './channels'
 
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
 
+window.scroll_bottom = function() {
+  if ($('#messages').length > 0) {
+    $('#messages').scrollTop($('#messages')[0].scrollHeight)
+  }
+}
 
+submit_message = function() {
+  $('#message_body').on('keydown', function(e) {
+    if (e.keyCode == 13) {
+      $('button').click();
+      e.target.value = "";
+    };
+  });
+};
 
 $(document).on('turbolinks:load', function() {
   $('.ui.dropdown').dropdown();
   $('.message .close').on('click', function() {
       $(this).closest('.message').transition('fade');
     });
+    submit_message();
+    scroll_bottom();
   })
--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -12,6 +12,7 @@ consumer.subscriptions.create("ChatroomChannel", {
   received(data) {
     // Called when there's incoming data on the websocket for this channel
    $('#message-container').append(data.mod_message)
+   scroll_bottom()
    //alert(data.mod_message);
   }
 });
